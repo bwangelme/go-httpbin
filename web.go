07f238a53ad42e7341bf2f65564d971264e9ae66
xuyundong <xuyundong@douban.com>
2018-10-15 23:34:30 +0800

    add /ip api

--- web.go
+++ web.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"rsc.io/quote"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func IPHandler(w http.ResponseWriter, r *http.Request) {
+	ip := w.Header().Get("X-Forwarded-For")
+	if len(ip) == 0 {
+		ip = r.RemoteAddr
+	}
+
+	js, err := json.Marshal(struct {
+		ip string
+	}{
+		ip: ip,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(js)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 func main() {
-	fmt.Println(quote.Hello())
+	r := mux.NewRouter()
+	r.HandleFunc("/ip", IPHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
